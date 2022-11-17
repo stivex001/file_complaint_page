c6e8f190f0282f6d3d7a800e0ba2f926ef0e7941
stivex001 <stephenadeyemo@gmail.com>
2022-11-17 18:34:32 +0100

    added responsiveness to hero page

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import HeroWrapper from './hero.styles'
+import React from "react";
+import HeroWrapper from "./hero.styles";
+import heroImage from "../../assets/hero.png"
 
 const Hero = () => {
   return (
-    <HeroWrapper>Hero</HeroWrapper>
-  )
-}
+    <HeroWrapper>
+      <div className="container">
+        <div className="hero-desc">
+          <h1>SUPPORT CENTER FOR YOUR HELP</h1>
+          <button>Contact Support</button>
+        </div>
+        <div className="hero-img">
+            <img src={heroImage} alt="" />
+        </div>
+      </div>
+    </HeroWrapper>
+  );
+};
 
-export default Hero
+export default Hero;
