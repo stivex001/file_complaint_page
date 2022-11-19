2376365da25ecdffed81a7a3b4fe59bb12f756b8
stivex001 <stephenadeyemo@gmail.com>
2022-11-19 01:27:47 +0100

    added a form modal

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HeroWrapper from "./hero.styles";
-import heroImage from "../../assets/hero.png"
+import heroImage from "../../assets/hero.png";
+import FormModal from "../modal/FormModal";
 
 const Hero = () => {
+  const [modalOpen, setmodalOpen] = useState(false);
+
+  useEffect(() => {
+    if (modalOpen) {
+      document.documentElement.style.overflow = "hidden";
+    }
+
+    return () => {
+      document.documentElement.style.overflowY = "scroll";
+    };
+  }, [modalOpen]);
+
+  const toggleModal = () => {
+    setmodalOpen((prev) => !prev);
+  };
+
   return (
-    <HeroWrapper>
-      <div className="container">
-        <div className="hero-desc">
-          <h1>SUPPORT CENTER FOR YOUR HELP</h1>
-          <button>Contact Support</button>
-        </div>
-        <div className="hero-img">
+    <>
+      <HeroWrapper>
+        <div className="container">
+          <div className="hero-desc">
+            <h1>SUPPORT CENTER FOR YOUR HELP</h1>
+            <button onClick={toggleModal}>Contact Support</button>
+          </div>
+          <div className="hero-img">
             <img src={heroImage} alt="" />
+          </div>
         </div>
-      </div>
-    </HeroWrapper>
+      </HeroWrapper>
+      <FormModal toggleModal={toggleModal} modalOpen={modalOpen}/>
+    </>
   );
 };
 
--- src/pages/Complaint.jsx
+++ src/pages/Complaint.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Article from '../components/article/Article'
-import Header from '../components/header/Header'
 import Hero from '../components/hero/Hero'
 import Support from '../components/support/Support'
 import Topic from '../components/topic/Topic'
@@ -9,7 +8,6 @@ import Topic from '../components/topic/Topic'
 const Complaint = () => {
   return (
     <>
-    <Header />
     <Hero />
     <Topic />
     <Support />
